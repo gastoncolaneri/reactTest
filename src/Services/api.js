731302fc02d0bba2196f4ffec25e6705561b8f73
unknown <colaneri_gaston@hotmail.com>
2022-02-03 01:14:44 -0300

    Updates

--- src/Services/api.js
+++
@@ -1,9 +0,0 @@
-const API_URL = "https://www.fruityvice.com/api/fruit/all";
-
-export default async function ApiRequest() {
-  const herokuHead = "https://cors-anywhere.herokuapp.com/";
-
-  let response = await fetch(herokuHead + API_URL);
-  let json = await response.json();
-  return json;
-}
--- src/utils/colors.js
+++
@@ -1,8 +0,0 @@
-export const colors = {
-  GREY: '#ececec',
-  WHITE: '#fff',
-  DARKGREY: '#646262',
-  GREYDARKMODE: '#333333',
-  BLACKDARKMODE: '#181818',
-  ORANGE: '#faad35',
-};
